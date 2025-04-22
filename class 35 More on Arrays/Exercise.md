9c16703bad4d5c398299e8e75991cd3ceadd4318
Lucy <youremail@example.com>
2025-04-22 15:29:09 +0530

    Q25 Second Max Element

---
+++ class 35 More on Arrays/Exercise.md
@@ -0,0 +1,7 @@
+Q 25. Second max element from array✅
+
+— Q 26. Reverse the array
+
+— Q 27. All zeroes to left and all ones to right
+
+— Q 28. Array left Rotation by 1
---
+++ class 35 More on Arrays/script.js
@@ -0,0 +1,16 @@
+// Q 25. Second max element from array
+// let arr = [12,33,23,233,43,22,11,233,40]
+// let max1 = arr[0];
+// let max2 = arr[1];
+// for(let i = 2; i < arr.length;i++){
+//     if(arr[i] >max1){
+//        max2 = max1;
+//        max1 = arr[i]
+//     }else if (arr[i] > max2 && arr[i] < max1) {
+//         max2 = arr[i];
+// }}
+// console.log("The max1 number is " + max1)
+// console.log("The max2 number is " + max2)
+
+// ------------------------------------- 
+
