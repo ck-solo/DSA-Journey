655bfe458daae2abedf5ef42b8d523c36507a0bc
Lucy <youremail@example.com>
2025-04-23 00:19:57 +0530

    Find second max, reverse  element in an array

--- class 35 More on Arrays/script.js
+++ class 35 More on Arrays/script.js
@@ -14,3 +14,13 @@
 
 // ------------------------------------- 
 
+// — Q 26. Reverse the array
+let arr = [1,2,3,4,5,6]
+let temp = new Array(arr.length)
+let i = 0;
+for(let j = arr.length-1;j>=0; j--){
+    temp[i] = arr[j]
+    i++
+}
+console.log(temp)
+
