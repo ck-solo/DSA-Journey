6186f9ce614434fb5c86e6d05da76bd80f2f9cc0
Lucy <solouser019@gmail.com>
2025-05-27 13:04:59 +0530

    Practice class 31 loops  in js

--- class 31 Loops in JS/script.js
+++ class 31 Loops in JS/script.js
@@ -1,32 +1,31 @@
 // Q.sum of digit
 // let a = Number(prompt("Enter a number"));
-// let sum = 0;
-// while(a>0){
-//     let rem = a % 10;
-//     sum = sum + rem;
-//     a = Math.floor(a/10);
-// }
+//  let sum = 0;
+//  while(a>0){
+//     let rem = a % 10
+//     sum = sum + rem
+//     a = Math.floor(a/10) ;
+// }  
 // console.log(sum)
 
 // Q2.reverse of number
-// let b = Number(prompt("Enter a number"));
-// let reverse = 0
+// let b = Number(prompt("Enter a number"))
+// let rev = 0;
 // while(b>0){
-//     let rem = b % 10;
-//     reverse = (reverse * 10) + rem;
+//     let rem = b % 10
+//     rev = (rev * 10) + rem
 //     b = Math.floor(b/10)
 // }
-// console.log(reverse)
+// console.log(rev) 
 
 // Q3.Automorphic number
-// let c = Number(prompt("Enter a number"));
-// let copy = c;
-// let sq = c * c; 
-// let count = 0;
-// while (c > 0) {
-//   count++; 
-//   c = Math.floor(c / 10);
-// }
-
-// if(sq % Math.pow(10, count) == copy) console.log("Automorphic Number")
-// else (console.log("Not a Automorphic number"))
+ let c = Number(prompt("Enter a number"))   
+ let copy = c;
+ let sqr = c * c;
+ let count = 0;
+ while(c>0){
+    count++
+    c = Math.floor(c/10)
+ }
+if(sqr%Math.pow(10, count)== copy) console.log("Automorphic Number")
+    else console.log("Not a Automorphic Number")
